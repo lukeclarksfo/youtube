f8d0e05a0f6daad21fae531c69359255335ef47a
Luke Clark <lukeclarksfo@gmail.com>
2024-07-06 07:23:29 +0200

    Update player.

---
+++ script.js
@@ -0,0 +1,18 @@
+const playPauseBtn = document.querySelector(".play-pause-btn")
+const videoContainer = document.querySelector(".video-container")
+const video = document.querySelector("video")
+
+playPauseBtn.addEventListener("click", togglePlay)
+
+function togglePlay()
+{
+    video.paused ? video.play() : video.pause();
+}
+
+video.addEventListener("play", () => {
+    videoContainer.classList.remove("paused");
+})
+
+video.addEventListener("pause", () => {
+    videoContainer.classList.add("paused");
+})
